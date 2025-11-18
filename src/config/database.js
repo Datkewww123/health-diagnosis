38fc19913e289ce2692cb6eadcc61d24faa1fbc8
Đạt Nguyễn <nguyendatz567@gmail.com>
2025-11-18 14:08:11 +0700

    update

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose'); //import thu vien mongoose
 
-const connectDB = async() =>{ // aynsc la ham bat dong bo
-    try{
-        await mongoose.connect('mongodb://localhost:27017/symptom_db', { // dung await de cho cac thao tac bat dong bo
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('connect sucessfully') // neu thanh cong
-    }
-    catch(error){
-        console.log('failed to connect!') // neu that bai
-}
+const connectDB = async () => { // aynsc la ham bat dong bo
+  try {
+    await mongoose.connect('mongodb://localhost:27017/User_db', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB; //cho phep import model nay vao file khac
