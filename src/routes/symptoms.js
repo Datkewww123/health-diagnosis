7e9069df2ffa9439297e78118e194acd5a874f91
Đạt Nguyễn <nguyendatz567@gmail.com>
2026-07-09 06:16:50 +0700

    refactor(routes): update symptoms routes with predict history endpoint

--- src/routes/symptoms.js
+++ src/routes/symptoms.js
@@ -4,6 +4,8 @@ const symptomsController = require('../controller/symptomsController');
 const {optionalAuth, verifyToken} = require('../middleware/auth');
 
 // Dự đoán bệnh theo triệu chứng (không cần login)
+router.get('/', symptomsController.getAllSymptoms);
 router.post('/check', optionalAuth, symptomsController.symptomsCheck);
 router.get('/history', verifyToken, symptomsController.getHistory);
+router.post('/history', verifyToken, symptomsController.savePredictHistory);
 module.exports = router;
