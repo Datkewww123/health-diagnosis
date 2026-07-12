6c170618b4e783b66c52f0ce1df7179af2b6a4b3
Đạt Nguyễn <nguyendatz567@gmail.com>
2026-07-12 08:52:35 +0700

    update: health-diagnosis-Backend/src/routes/user.js

---
+++ health-diagnosis-Backend/src/routes/user.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const userController = require('../controller/userController');
+const router = express.Router();
+
+const {verifyToken} = require('../middleware/auth');
+
+// lay thong tin cua user
+router.get('/getUser', verifyToken, userController.getUser);
+
+// update user
+router.patch('/updateUser', verifyToken, userController.updateUser);
+
+module.exports = router;
