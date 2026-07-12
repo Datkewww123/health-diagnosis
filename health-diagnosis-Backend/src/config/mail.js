bcfd0a7e362a5a7ec8147b7cf109ead3e9bfce03
Đạt Nguyễn <nguyendatz567@gmail.com>
2026-07-12 08:52:08 +0700

    update: health-diagnosis-Backend/src/config/mail.js

---
+++ health-diagnosis-Backend/src/config/mail.js
@@ -0,0 +1,30 @@
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.GMAIL_CLIENT_ID,
+  process.env.GMAIL_CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
+
+oAuth2Client.setCredentials({
+  refresh_token: process.env.GMAIL_REFRESH_TOKEN,
+}); 
+
+async function createTransporter() {
+  const accessToken = await oAuth2Client.getAccessToken();
+
+  return nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.GMAIL_SENDER,
+      clientId: process.env.GMAIL_CLIENT_ID,
+      clientSecret: process.env.GMAIL_CLIENT_SECRET,
+      refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+      accessToken: accessToken?.token,
+    },
+  });
+}
+
+module.exports = createTransporter;
